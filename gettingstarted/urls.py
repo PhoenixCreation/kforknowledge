32d1e68c24c21c39b732ce3ab217c25f88dccaa0
phoenix <phs28052001@gmail.com>
2020-07-27 20:19:33 +0530

    intial commit

---
+++ gettingstarted/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+
+from django.contrib import admin
+import knowledgeapp.views as view
+
+admin.autodiscover()
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", view.index,name="index"),
+]
---
+++ gettingstarted/wsgi.py
@@ -0,0 +1,9 @@
+
+
+import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.settings")
+
+from django.core.wsgi import get_wsgi_application
+
+application = get_wsgi_application()
