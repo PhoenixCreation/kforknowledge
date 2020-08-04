3ad99dace2f54c416d9df6071c74587f7adc5896
phoenix <phs28052001@gmail.com>
2020-08-04 11:34:52 +0530

    added feedback form(now workig with kfork ID)

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -89,4 +89,12 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "/static/"
 
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'DE.sem5.kfork@gmail.com'
+EMAIL_HOST_PASSWORD = 'kfork#123'
+EMAIL_PORT = 587
+
+
 django_heroku.settings(locals())
--- knowledgeapp/views.py
+++ knowledgeapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import *
+from django.core.mail import EmailMessage
+
 
 def index(req):
     return render(req,"index.html")
@@ -18,6 +20,13 @@ def course(req,id):
 
 def contact(req):
     if req.method == "POST":
+        fname = req.POST['first-name']
+        lname = req.POST['last-name']
+        email = req.POST['email']
+        message = req.POST['message']
+        msgbody = "name: "+fname+" : "+lname+" email: "+email+" msg: "+message+"....."
+        emailphnx = EmailMessage('Feedback of user',msgbody,to=['phoenixcreationweb@gmail.com'])
+        emailphnx.send()
         info={
             "msg": "Thank you for your valuable feedback."
         }
